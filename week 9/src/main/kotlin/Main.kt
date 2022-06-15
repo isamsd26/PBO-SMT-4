1cdbcda26ddb5f6bdc085b4b3f4daebc3d501dd8
isamsd26 <91447964+isamsd26@users.noreply.github.com>
2022-06-15 20:55:20 +0700

    week 8-9

---
+++ week 9/src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+package com.includehelp.basic
+
+import java.util.*
+
+
+//Function to check Palindrome Number
+fun isPalindrome(number: Int): Boolean {
+    var isPalindromeNumber = false
+    var sum = 0
+    var tempNum = number
+
+    while (tempNum > 0) {
+        val r = tempNum % 10
+        sum = sum * 10 + r
+        tempNum /= 10
+    }
+    if (sum == number) {
+        isPalindromeNumber = true
+    }
+    return isPalindromeNumber
+}
+
+//Main Function, Entry Point of Program
+fun main(arg: Array<String>) {
+    val sc = Scanner(System.`in`)
+
+    //  Input Number
+    println("Enter Number  : ")
+    val num: Int = sc.nextInt()
+
+    //Call Function to check Number
+    if (isPalindrome(num))
+        println("$num is a Palindrome Number")
+    else
+        println("$num is not a Palindrome Number")
+}
